b4059c0b7fee374b288be5fb6eacb44ceb6015d7
Yap8778 <junsong1341@gmail.com>
2025-04-12 18:37:38 +0800

    Delete Emonie

--- Emonie (Mean Stack)/mean-course/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './Register/register.component';
-import { LoginComponent } from './Login/login.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/register', pathMatch: 'full' },
-    { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent }
-];
